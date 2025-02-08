03ddf79cbc708183f0439ab66d1ed6938bfad949
Jakob Pistohl <gh@jp-bits.de>
2025-02-08 16:57:11 +0100

    Add showcase method

--- lukeroberts.py
+++ lukeroberts.py
@@ -18,28 +18,38 @@ async def print_svc(addr):
 
     await client.disconnect()
 
-async def test(addr):
+async def showcase(addr):
     client = await connect(addr)
     if not client:
         print("ERROR: connection failed")
         return
 
-    # uuid = "44092844-0567-11e6-b862-0002a5d5c51b"
-    # unknown. write_value = 00 -> 0x15
+    name = "new profile" # custom name of the new profile
 
-    # uuid = "44092848-0567-11e6-b862-0002a5d5c51b"
-    # unknown. rsvd value: 2; write_value = 0x3200
+    uuid = "44092843-0567-11e6-b862-0002a5d5c51b"
+    MAX_LEN = 40 # maximum length: 20 characters = 40 hex characters
+    name_hex = name.lower().encode("utf-8").hex() # hex encode name
+    name_hex += '0' * (MAX_LEN - len(name_hex)) # pad hex
+    name_hex = name_hex[0:40] # ensure maximum length
+    write_value = bytes.fromhex(f"01fe0001000000008c0a{'0' * 296}{name_hex}")
+    await client.write_char(uuid, write_value, response=False)
 
-    # unknown
-    # uuid = "44092842-0567-11e6-b862-0002a5d5c51b"
-    # write_value = 0x0200
+    await asyncio.sleep(1)
 
-    # value to nothing?
-    # write_value = 0x648c0a00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+    uuid = "44092844-0567-11e6-b862-0002a5d5c51b"
+    write_value = bytes.fromhex(f"00") # turn lamp off
+    await client.write_char(uuid, write_value, response=False)
 
-    # rsvd_value = await client.get_char(uuid)
-    # if rsvd_value is not None:
-    #     print(f"Characteristic value: {value}")
+    await client.disconnect()
+
+async def turn_off(addr):
+    client = await connect(addr)
+    if not client:
+        print("ERROR: connection failed")
+        return
+
+    uuid = "44092844-0567-11e6-b862-0002a5d5c51b"
+    write_value = bytes.fromhex(f"00") # turn lamp off
 
     await client.write_char(uuid, write_value, response=False)
 
